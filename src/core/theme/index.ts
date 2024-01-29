cc36860cbf8216a37c942cab9e1f0c397bcaf38c
Andrew Lenhart <andrew.code21@gmail.com>
2024-01-28 21:53:28 -0500

    feat(styles): refine color tokens and swatches

--- src/core/theme/index.ts
+++ src/core/theme/index.ts
@@ -1,3 +1,4 @@
 export * from './theme.provider';
 export * from './theme.context';
+export * from './theme.tokens';
 export * from './types';
---
+++ src/core/theme/theme.tokens.ts
@@ -0,0 +1,39 @@
+export type TBDXAlpha = 'A0' | 'A1' | 'A2' | 'A3' | 'A4' | 'A5';
+export type TBDXShade = '0' | '1' | '2' | '3' | '4' | '5' | '6' | '7' | '8' | '9';
+
+export type TBDXAccentVariantType = 'base' | 'base-interactive';
+export type TBDXAccentVariantKey = `${TBDXColor}-${TBDXAccentVariantType}`;
+export type TBDXAccentShades = `${TBDXAccentVariantKey}-${TBDXShade | TBDXAlpha}`;
+
+export type TBDXSurfaceVariantType = 'base' | 'base-interactive' | 'invert' | 'invert-interactive';
+export type TBDXSurfaceVariantKey = `surface-${TBDXSurfaceVariantType}`;
+export type TBDXSurfaceShades = `${TBDXSurfaceVariantKey}-${TBDXShade}`;
+
+export type TBDXColor =
+  | 'orange'
+  | 'yellow'
+  | 'green'
+  | 'cyan'
+  | 'blue'
+  | 'indigo'
+  | 'purple'
+  | 'violet'
+  | 'pink'
+  | 'magenta'
+  | 'red';
+
+export const DATA_TBDX_ALPHA: TBDXAlpha[] = ['A0', 'A1', 'A2', 'A3', 'A4', 'A5'];
+export const DATA_TBDX_SHADE: TBDXShade[] = ['0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+export const DATA_TBDX_COLOR: TBDXColor[] = [
+  'orange',
+  'yellow',
+  'green',
+  'cyan',
+  'blue',
+  'indigo',
+  'purple',
+  'violet',
+  'pink',
+  'magenta',
+  'red',
+];
